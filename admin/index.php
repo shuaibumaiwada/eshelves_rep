a4b883075b9b269bce642d21324a3d5453a790bc
Shuaibu Maiwada <shuaibumaiwada@gmail.com>
2024-12-28 14:49:50 +0100

    index changes

--- admin/index.php
+++ admin/index.php
@@ -50,7 +50,7 @@
       opacity: 0 !important
     }
   </style>
-  <script src="./admindash_sales/token_create.js"></script><script type="text/javascript" async="" src="./admindash_sales/identify_45dd5971.js"></script><script type="text/javascript" async="" src="./admindash_sales/main.MWE1OTI4NzI4Mw.js" data-id="CC6UAQBC77U7GVKHLC4G"></script><script type="text/javascript" async="" src="./admindash_sales/js"></script><script type="text/javascript" async="" src="./admindash_sales/js(1)"></script><script async="" src="./admindash_sales/insight.old.min.js"></script><script async="" src="./admindash_sales/main.7d8116bd.js"></script><script type="text/javascript" async="" src="./admindash_sales/events.js"></script><script type="text/javascript" async="" src="./admindash_sales/insight.min.js"></script><script type="text/javascript" async="" src="./admindash_sales/core.js"></script><script type="text/javascript" async="" src="./admindash_sales/analytics.js"></script><script type="text/javascript" async="" src="./admindash_sales/js(2)"></script><script async="" src="./admindash_sales/gtm.js"></script><script async="" src="./admindash_sales/analytics.js"></script><script>
+<script>
     (function(a, s, y, n, c, h, i, d, e) {
       s.className += ' ' + y;
       h.start = 1 * new Date;
